5e8b7ee8191f56997d9726680b7efc959a8343c6
EugeneXpertis <eugene.xpertis@gmail.com>
2025-03-24 14:17:25 +0800

    removing useless things

--- APP/API/get_students_api.py
+++ APP/API/get_students_api.py
@@ -6,61 +6,6 @@ from database import get_db
 
 get_students_api_router = APIRouter()
 
-@get_students_api_router.get("/api/senior-high-students")
-def get_senior_high_students(
-    db: Session = Depends(get_db),
-    current_user: models.User = Depends(auth.get_current_user),
-    page: int = 1,
-    page_size: int = 10
-):
-    offset = (page - 1) * page_size
-    students = db.query(models.SeniorHighStudents).offset(offset).limit(page_size).all()
-    total_students = db.query(models.SeniorHighStudents).count()
-
-    return {
-        "students": [
-            {
-                "year": student.year,
-                "strand": student.strand,
-                "previous_school": student.previous_school,
-                "age": student.age,
-                "full_address": student.full_address,
-            }
-            for student in students
-        ],
-        "total": total_students,
-        "page": page,
-        "page_size": page_size
-    }
-
-@get_students_api_router.get("/api/college-students")
-def get_college_students(
-    db: Session = Depends(get_db),
-    current_user: models.User = Depends(auth.get_current_user),
-    page: int = 1,
-    page_size: int = 10
-):
-    offset = (page - 1) * page_size
-    students = db.query(models.CollegeStudents).offset(offset).limit(page_size).all()
-    total_students = db.query(models.CollegeStudents).count()
-
-    return {
-        "students": [
-            {
-                "year": student.year,
-                "course": student.course,
-                "strand": student.strand,
-                "previous_school": student.previous_school,
-                "age": student.age,
-                "full_address": student.full_address,
-            }
-            for student in students
-        ],
-        "total": total_students,
-        "page": page,
-        "page_size": page_size
-    }
-
 
 @get_students_api_router.get("/api/senior-high-students/count")
 def get_senior_high_student_count(db: Session = Depends(get_db), current_user: models.User = Depends(auth.get_current_user)):
